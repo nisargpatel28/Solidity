f5694a7bf3857abcc3bd59cbe3047bb725f0f3fd
Nisarg Patel <patelnisarg.pn@gmail.com>
2025-10-27 06:07:32 -0400

    Twitter solution added

--- calculator/Solutions/7-11-BasicTwitter-Solution.sol
+++ calculator/Solutions/7-11-BasicTwitter-Solution.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.0;
 // 2. Create a mapping between user and tweet ✅
 // 3. Add function to create a tweet and save it in mapping ✅
 // 4. Create a function to get Tweet ✅
-// 5. Add array of tweets ✅
+// 5. Add array of tweets. ✅
 
 contract Twitter {
 
