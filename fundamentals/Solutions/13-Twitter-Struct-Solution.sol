a1054df743b9097c64c7a33f48d766776fe42a2d
Nisarg Patel <patelnisarg.pn@gmail.com>
2025-12-18 01:53:06 -0500

    Structure solution added

--- fundamentals/Solutions/13-Twitter-Struct-Solution.sol
+++ fundamentals/Solutions/13-Twitter-Struct-Solution.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.0;
 // 1️⃣ Define a Tweet Struct with author, content, timestamp, likes
 // 2️⃣ Add the struct to array
 // 3️⃣ Test Tweets
+// Added contract Twitter
 
 contract Twitter {
 
