e2079c5d116c52bc802e8ce0dce20a8d39bf3eb3
Nisarg Patel <patelnisarg.pn@gmail.com>
2025-12-23 05:12:32 -0500

    User creation prevented

--- fundamentals/Solutions/27-Interaction-Solution-User.sol
+++ fundamentals/Solutions/27-Interaction-Solution-User.sol
@@ -11,12 +11,4 @@ contract User {
 
     mapping(address => Player) public players;
 
-    function createUser(address userAddress, string memory username) external {
-        require(players[userAddress].playerAddress == address(0), "User already exists");
-
-        Player storage newPlayer = players[userAddress];
-        newPlayer.playerAddress = userAddress;
-        newPlayer.username = username;
-        newPlayer.score = 0;
-    }
 }
